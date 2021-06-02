dd117181b683bea49cdd54de5a14d6a2e9df91b0
shyam <shyamiitb@gmail.com>
2021-06-02 13:30:27 -0700

    Fix bug

--- src/main/java/com/example/hellocd/HelloCdApplication.java
+++ src/main/java/com/example/hellocd/HelloCdApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellocdApplication {
+@RestController
+@EnableAutoConfiguration
+public class HelloCdApplication {
 
 
 	@RequestMapping("/")
@@ -16,7 +18,7 @@ public class HellocdApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellocdApplication.class, args);
+		SpringApplication.run(HelloCdApplication.class, args);
 	}
 
 }
